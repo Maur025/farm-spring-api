16e804d938114bd1f6f7d1bf33b61f791e28a29e
Maur025 <mmoyapalli@gmail.com>
2025-08-26 17:46:04 -0400

    add entity Person as demo

---
+++ farm-service/src/main/java/com/kernotec/farm/jpa/service/PersonService.java
@@ -0,0 +1,26 @@
+package com.kernotec.farm.jpa.service;
+
+import com.kernotec.core.jpa.repository.BaseRepository;
+import com.kernotec.core.jpa.service.BaseServiceImpl;
+import com.kernotec.farm.jpa.entity.Person;
+import com.kernotec.farm.jpa.repository.PersonRepository;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@AllArgsConstructor
+@Service
+public class PersonService extends BaseServiceImpl<Person, UUID> {
+
+    private final PersonRepository repository;
+
+    @Override
+    protected String resourceName() {
+        return "Person";
+    }
+
+    @Override
+    protected BaseRepository<Person, UUID> repository() {
+        return repository;
+    }
+}
