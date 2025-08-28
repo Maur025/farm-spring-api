820816e45e8d05e7dff79200dd0ca8e4d0421e70
Maur025 <mmoyapalli@gmail.com>
2025-08-28 09:48:38 -0400

    command to create follow,friend,group and publishing

---
+++ farm-service/src/main/java/com/kernotec/farm/command/group/GroupCreateCmd.java
@@ -0,0 +1,50 @@
+package com.kernotec.farm.command.group;
+
+import com.kernotec.core.command.AbstractTransactionalRequiredCommand;
+import com.kernotec.farm.jpa.entity.Group;
+import com.kernotec.farm.jpa.service.GroupService;
+import jakarta.validation.constraints.NotNull;
+import java.util.UUID;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class GroupCreateCmd extends
+    AbstractTransactionalRequiredCommand<GroupCreateCmd.Request, UUID>
+{
+
+    private final GroupService groupService;
+
+    @Override
+    protected UUID run(Request request) {
+        Group group = new Group();
+
+        group.setName(request.getName());
+        group.setJoin(request.isJoin());
+        group.setRegionId(request.getRegionId());
+        group.setActivityId(request.getActivityId());
+        group.setActivityTypeId(request.getActivityTypeId());
+
+        group = groupService.save(group);
+        return group.getId();
+    }
+
+    @Builder
+    @Getter
+    public static class Request {
+
+        @NotNull
+        private final String name;
+        @NotNull
+        private final boolean isJoin;
+        @NotNull
+        private final UUID regionId;
+        @NotNull
+        private final UUID activityId;
+        @NotNull
+        private final UUID activityTypeId;
+    }
+}
---
+++ farm-service/src/test/java/com/kernotec/farm/command/comment/CommentCreateCmdTest.java
@@ -0,0 +1,7 @@
+package com.kernotec.farm.command.comment;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CommentCreateCmdTest {
+
+}
