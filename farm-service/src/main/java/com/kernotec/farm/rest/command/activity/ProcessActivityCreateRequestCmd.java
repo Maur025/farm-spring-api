36f139a24517e0568ea08bf49328134a78c4e6e0
Maur025 <mmoyapalli@gmail.com>
2025-08-30 02:51:28 -0400

    create activity with need relations

--- farm-service/src/main/java/com/kernotec/farm/rest/command/activity/ProcessActivityCreateRequestCmd.java
+++ farm-service/src/main/java/com/kernotec/farm/rest/command/activity/ProcessActivityCreateRequestCmd.java
@@ -2,7 +2,19 @@ package com.kernotec.farm.rest.command.activity;
 
 import com.kernotec.core.command.AbstractTransactionalRequiredCommand;
 import com.kernotec.farm.command.activity.ActivityCreateCmd;
+import com.kernotec.farm.command.comment.CommentCreateCmd;
+import com.kernotec.farm.command.follow.FollowCreateCmd;
+import com.kernotec.farm.command.friend.FriendCreateCmd;
+import com.kernotec.farm.command.group.GroupCreateCmd;
+import com.kernotec.farm.command.publishing.PublishingCreateCmd;
+import com.kernotec.farm.command.reaction.ReactionCreateCmd;
 import com.kernotec.farm.rest.dto.request.activity.ActivityCreateRequest;
+import com.kernotec.farm.rest.dto.request.comment.CommentCreateRequest;
+import com.kernotec.farm.rest.dto.request.follow.FollowCreateRequest;
+import com.kernotec.farm.rest.dto.request.friend.FriendCreateRequest;
+import com.kernotec.farm.rest.dto.request.group.GroupCreateRequest;
+import com.kernotec.farm.rest.dto.request.publishing.PublishingCreateRequest;
+import com.kernotec.farm.rest.dto.request.reaction.ReactionCreateRequest;
 import jakarta.validation.constraints.NotNull;
 import java.util.UUID;
 import lombok.Builder;
@@ -17,16 +29,141 @@ public class ProcessActivityCreateRequestCmd extends
 {
 
     private final ActivityCreateCmd activityCreateCmd;
+    private final PublishingCreateCmd publishingCreateCmd;
+    private final ReactionCreateCmd reactionCreateCmd;
+    private final CommentCreateCmd commentCreateCmd;
+    private final GroupCreateCmd groupCreateCmd;
+    private final FriendCreateCmd friendCreateCmd;
+    private final FollowCreateCmd followCreateCmd;
 
     @Override
     protected UUID run(Request request) {
         ActivityCreateRequest activityRequest = request.getActivityRequest();
 
-        return activityCreateCmd.withRequest(ActivityCreateCmd.Request.builder()
+        UUID activityId = activityCreateCmd.withRequest(ActivityCreateCmd.Request.builder()
                 .link(activityRequest.getLink())
                 .activityDate(activityRequest.getActivityDate())
                 .accountId(activityRequest.getAccountId())
                 .activityTypeId(activityRequest.getActivityTypeId())
+                .build())
+            .execute();
+
+        createPublishingRelation(
+            activityRequest.getPublishing(), activityId, activityRequest.getActivityTypeId());
+
+        createReactionRelation(
+            activityRequest.getReaction(), activityId, activityRequest.getActivityTypeId());
+
+        createCommentRelation(
+            activityRequest.getComment(), activityId, activityRequest.getActivityTypeId());
+
+        createGroupRelation(
+            activityRequest.getGroup(), activityId, activityRequest.getActivityTypeId());
+
+        createFriendRelation(
+            activityRequest.getFriend(), activityId, activityRequest.getActivityTypeId());
+
+        createFollowRelation(
+            activityRequest.getFollow(), activityId, activityRequest.getActivityTypeId());
+
+        return activityId;
+    }
+
+    public void createPublishingRelation(PublishingCreateRequest publishingRequest, UUID activityId,
+        UUID activityTypeId)
+    {
+        if (publishingRequest == null || publishingRequest.getPublishingContextId() == null) {
+            return;
+        }
+
+        publishingCreateCmd.withRequest(PublishingCreateCmd.Request.builder()
+                .description(publishingRequest.getDescription())
+                .publishingTypeId(publishingRequest.getPublishingTypeId())
+                .publishingContextId(publishingRequest.getPublishingContextId())
+                .activityId(activityId)
+                .activityTypeId(activityTypeId)
+                .build())
+            .execute();
+    }
+
+    public void createReactionRelation(ReactionCreateRequest reactionRequest, UUID activityId,
+        UUID activityTypeId)
+    {
+        if (reactionRequest == null || reactionRequest.getReactionTypeId() == null) {
+            return;
+        }
+
+        reactionCreateCmd.withRequest(ReactionCreateCmd.Request.builder()
+                .reactionTypeId(reactionRequest.getReactionTypeId())
+                .activityId(activityId)
+                .activityTypeId(activityTypeId)
+                .build())
+            .execute();
+    }
+
+    public void createCommentRelation(CommentCreateRequest commentRequest, UUID activityId,
+        UUID activityTypeId)
+    {
+        if (commentRequest == null || commentRequest.getComment() == null) {
+            return;
+        }
+
+        commentCreateCmd.withRequest(CommentCreateCmd.Request.builder()
+                .comment(commentRequest.getComment())
+                .isAgreeComment(
+                    commentRequest.getIsAgreeComment() != null && commentRequest.getIsAgreeComment())
+                .activityId(activityId)
+                .activityTypeId(activityTypeId)
+                .build())
+            .execute();
+    }
+
+    public void createGroupRelation(GroupCreateRequest groupRequest, UUID activityId,
+        UUID activityTypeId)
+    {
+        if (groupRequest == null || groupRequest.getName() == null) {
+            return;
+        }
+
+        groupCreateCmd.withRequest(GroupCreateCmd.Request.builder()
+                .name(groupRequest.getName())
+                .isJoin(groupRequest.getIsJoin() != null && groupRequest.getIsJoin())
+                .regionId(groupRequest.getRegionId())
+                .activityId(activityId)
+                .activityTypeId(activityTypeId)
+                .build())
+            .execute();
+    }
+
+    public void createFriendRelation(FriendCreateRequest friendRequest, UUID activityId,
+        UUID activityTypeId)
+    {
+        if (friendRequest == null || friendRequest.getFriendName() == null) {
+            return;
+        }
+
+        friendCreateCmd.withRequest(FriendCreateCmd.Request.builder()
+                .name(friendRequest.getFriendName())
+                .status(friendRequest.getStatus())
+                .type(friendRequest.getTypeFriendShip())
+                .activityId(activityId)
+                .activityTypeId(activityTypeId)
+                .build())
+            .execute();
+    }
+
+    public void createFollowRelation(FollowCreateRequest followRequest, UUID activityId,
+        UUID activityTypeId)
+    {
+        if (followRequest == null || followRequest.getName() == null) {
+            return;
+        }
+
+        followCreateCmd.withRequest(FollowCreateCmd.Request.builder()
+                .name(followRequest.getName())
+                .isFollowing(followRequest.getIsFollowing() != null && followRequest.getIsFollowing())
+                .activityId(activityId)
+                .activityTypeId(activityTypeId)
                 .build())
             .execute();
     }
