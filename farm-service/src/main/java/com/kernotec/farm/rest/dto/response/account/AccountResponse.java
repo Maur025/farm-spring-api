5dce681618cb82cb0746f1aa92fa7a8ae4f0b066
Maur025 <mmoyapalli@gmail.com>
2025-08-29 23:14:23 -0400

    update chips response, to device api

--- farm-service/src/main/java/com/kernotec/farm/rest/dto/response/account/AccountResponse.java
+++ farm-service/src/main/java/com/kernotec/farm/rest/dto/response/account/AccountResponse.java
@@ -3,6 +3,7 @@ package com.kernotec.farm.rest.dto.response.account;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.kernotec.core.rest.dto.response.data.EntityResponse;
+import com.kernotec.farm.rest.dto.response.person.PersonResponse;
 import com.kernotec.farm.rest.dto.response.social.network.SocialNetworkResponse;
 import java.util.UUID;
 import lombok.Getter;
@@ -17,6 +18,8 @@ public class AccountResponse extends EntityResponse {
     private String password;
 
     private UUID personId;
+    private PersonResponse person;
+
     private UUID chipId;
 
     private UUID socialNetworkId;
--- farm-service/src/main/java/com/kernotec/farm/rest/dto/response/chip/ChipResponse.java
+++ farm-service/src/main/java/com/kernotec/farm/rest/dto/response/chip/ChipResponse.java
@@ -3,8 +3,10 @@ package com.kernotec.farm.rest.dto.response.chip;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.kernotec.core.rest.dto.response.data.EntityResponse;
+import com.kernotec.farm.rest.dto.response.account.AccountResponse;
 import com.kernotec.farm.rest.dto.response.operator.OperatorResponse;
 import com.kernotec.farm.rest.dto.response.registration.person.RegistrationPersonResponse;
+import java.util.Set;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,4 +26,5 @@ public class ChipResponse extends EntityResponse {
     private RegistrationPersonResponse registrationPerson;
 
     private UUID deviceId;
+    private Set<AccountResponse> accounts;
 }
