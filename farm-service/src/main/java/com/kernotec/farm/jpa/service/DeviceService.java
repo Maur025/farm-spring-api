282596fddf02382f6aa9dc71e24833ed09a5b126
Maur025 <mmoyapalli@gmail.com>
2025-09-02 11:25:04 -0400

    refactor: change filter of social networks, add seeder of colors to social_networks, fix response of social_networks

--- farm-service/src/main/java/com/kernotec/farm/jpa/service/DeviceService.java
+++ farm-service/src/main/java/com/kernotec/farm/jpa/service/DeviceService.java
@@ -2,12 +2,19 @@ package com.kernotec.farm.jpa.service;
 
 import com.kernotec.core.jpa.repository.BaseRepository;
 import com.kernotec.core.jpa.service.BaseServiceImpl;
+import com.kernotec.farm.jpa.entity.Account;
 import com.kernotec.farm.jpa.entity.Device;
 import com.kernotec.farm.jpa.repository.DeviceRepository;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -27,19 +34,27 @@ public class DeviceService extends BaseServiceImpl<Device, UUID> {
         return repository;
     }
 
-    public List<Device> findAllByKeyword(String keyword) {
-        String pattern = keyword == null ? null : "%" + keyword.toLowerCase() + "%";
+    public Page<Device> findAllByKeyword(String keyword, UUID socialNetworkId, Pageable pageable) {
+        String pattern = keyword == null || keyword.isBlank() ? null : keyword.toLowerCase() + "%";
 
-        return repository.findAll((Specification<Device>) (root, query, cb) -> {
-            if (pattern == null) {
-                return cb.conjunction();
-            } else {
-                return cb.or(
-                    cb.like(cb.lower(root.get("name")), pattern),
-                    cb.like(cb.lower(root.get("deviceNumber")), pattern)
-                );
-            }
-        });
+        return repository.findAll(
+            (Specification<Device>) (root, query, cb) -> {
+                List<Predicate> predicateList = new ArrayList<>();
+
+                if (socialNetworkId != null) {
+                    Join<Device, Account> accountJoin = root.join("accounts", JoinType.INNER);
+                    predicateList.add(
+                        cb.equal(accountJoin.get("socialNetworkId"), socialNetworkId));
+                }
+
+                if (keyword != null) {
+                    predicateList.add(cb.or(cb.like(cb.lower(root.get("deviceNumber")), pattern)));
+                }
+
+                query.distinct(true);
+                return cb.and(predicateList.toArray(Predicate[]::new));
+            }, pageable
+        );
     }
 
     public Optional<Device> findByDeviceNumber(String deviceNumber) {
--- farm-service/src/main/java/com/kernotec/farm/jpa/service/SocialNetworkService.java
+++ farm-service/src/main/java/com/kernotec/farm/jpa/service/SocialNetworkService.java
@@ -5,9 +5,12 @@ import com.kernotec.core.jpa.service.BaseServiceImpl;
 import com.kernotec.farm.exception.SocialNetworkException;
 import com.kernotec.farm.jpa.entity.SocialNetwork;
 import com.kernotec.farm.jpa.repository.SocialNetworkRepository;
+import jakarta.persistence.criteria.JoinType;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -34,6 +37,21 @@ public class SocialNetworkService extends BaseServiceImpl<SocialNetwork, UUID> {
     public SocialNetwork findByCodeThrow(String code) {
         return findByCode(code).orElseThrow(
             () -> new SocialNetworkException(
-                "code.not.found", "'" + code + "'", HttpStatus.NOT_FOUND.value()));
+                "code.not.found", "'" + code + "'",
+                HttpStatus.NOT_FOUND.value()
+            ));
+    }
+
+    public List<SocialNetwork> findAllWithFilters(Boolean isHasAccounts) {
+        if (isHasAccounts == null || isHasAccounts.equals(Boolean.FALSE)) {
+            return repository.findAll();
+        }
+
+        return repository.findAll((Specification<SocialNetwork>) (root, query, cb) -> {
+            root.join("accounts", JoinType.INNER);
+
+            query.distinct(true);
+            return cb.conjunction();
+        });
     }
 }
