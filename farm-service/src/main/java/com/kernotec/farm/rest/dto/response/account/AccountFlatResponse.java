50f690556dcf00ed6346656785af4a3f37533931
Maur025 <mmoyapalli@gmail.com>
2025-09-02 13:21:54 -0400

    refactor: changes in account response, adding account response flat

--- farm-service/src/main/java/com/kernotec/farm/rest/dto/response/account/AccountFlatResponse.java
+++ farm-service/src/main/java/com/kernotec/farm/rest/dto/response/account/AccountFlatResponse.java
@@ -4,10 +4,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.kernotec.core.rest.dto.response.data.EntityResponse;
 import com.kernotec.farm.jpa.enums.AccountTypeEnum;
-import com.kernotec.farm.rest.dto.response.device.DeviceResponse;
-import com.kernotec.farm.rest.dto.response.person.PersonResponse;
-import com.kernotec.farm.rest.dto.response.social.network.SocialNetworkResponse;
-import java.util.Set;
 import java.util.UUID;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,18 +11,11 @@ import lombok.Setter;
 @Getter
 @Setter
 @JsonInclude(Include.NON_NULL)
-public class AccountResponse extends EntityResponse {
+public class AccountFlatResponse extends EntityResponse {
 
     private String username;
     private String password;
-
     private AccountTypeEnum type;
-
     private UUID personId;
-    private PersonResponse person;
-
     private UUID socialNetworkId;
-    private SocialNetworkResponse socialNetwork;
-
-    private Set<DeviceResponse> devices;
 }
