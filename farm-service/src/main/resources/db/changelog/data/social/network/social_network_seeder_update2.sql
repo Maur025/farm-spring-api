9d525b0dd46812c28ed4911cb4858469b9663f51
Maur025 <mmoyapalli@gmail.com>
2025-09-02 10:05:01 -0400

    feat: add seeder to request states and update seeder of social networks

---
+++ farm-service/src/main/resources/db/changelog/data/social/network/social_network_seeder_update2.sql
@@ -0,0 +1,27 @@
+UPDATE social_networks
+SET icon = 'fa-brands fa-facebook-f'
+WHERE code = 'FACEBOOK';
+
+UPDATE social_networks
+SET icon = 'fa-brands fa-tiktok'
+WHERE code = 'TIKTOK';
+
+UPDATE social_networks
+SET icon = 'fa-brands fa-instagram'
+WHERE code = 'INSTAGRAM';
+
+UPDATE social_networks
+SET icon = 'fa-brands fa-x-twitter'
+WHERE code = 'X';
+
+UPDATE social_networks
+SET icon = 'fa-brands fa-youtube'
+WHERE code = 'YOUTUBE';
+
+UPDATE social_networks
+SET icon = 'fa-solid fa-at'
+WHERE code = 'CORREO';
+
+UPDATE social_networks
+SET icon = 'fa-brands fa-whatsapp'
+WHERE code = 'WHATSAPP';
