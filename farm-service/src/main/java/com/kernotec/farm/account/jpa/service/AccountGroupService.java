34429cb2591cd93ca6e3a1616567cc8ddb4a5d00
Maur025 <mmoyapalli@gmail.com>
2025-09-04 23:24:50 -0400

    feat: add account group controller, add services, modify responses adding new fields

--- farm-service/src/main/java/com/kernotec/farm/account/jpa/service/AccountGroupService.java
+++ farm-service/src/main/java/com/kernotec/farm/account/jpa/service/AccountGroupService.java
@@ -2,10 +2,19 @@ package com.kernotec.farm.account.jpa.service;
 
 import com.kernotec.core.jpa.repository.BaseRepository;
 import com.kernotec.core.jpa.service.BaseServiceImpl;
+import com.kernotec.farm.account.jpa.entity.Account;
 import com.kernotec.farm.account.jpa.entity.AccountGroup;
 import com.kernotec.farm.account.jpa.repository.AccountGroupRepository;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
@@ -22,5 +31,29 @@ public class AccountGroupService extends BaseServiceImpl<AccountGroup, UUID> {
     @Override
     protected BaseRepository<AccountGroup, UUID> repository() {
         return repository;
+    }
+
+    public Page<AccountGroup> findAllWithFilters(UUID accountId, UUID socialNetworkId,
+        Pageable pageable)
+    {
+        return repository.findAll(
+            (Specification<AccountGroup>) (root, query, cb) -> {
+                List<Predicate> predicateList = new ArrayList<>();
+
+                if (accountId != null) {
+                    predicateList.add(cb.equal(root.get("accountId"), accountId));
+                }
+
+                if (socialNetworkId != null) {
+                    Join<AccountGroup, Account> accountJoin = root.join("account", JoinType.INNER);
+
+                    predicateList.add(
+                        cb.equal(accountJoin.get("socialNetworkId"), socialNetworkId));
+                }
+
+                query.distinct(true);
+                return cb.and(predicateList.toArray(jakarta.persistence.criteria.Predicate[]::new));
+            }, pageable
+        );
     }
 }
--- farm-service/src/main/java/com/kernotec/farm/account/rest/ApiSpec.java
+++ farm-service/src/main/java/com/kernotec/farm/account/rest/ApiSpec.java
@@ -23,4 +23,24 @@ public class ApiSpec {
         private PersonSpec() {
         }
     }
+
+    public static final class FriendSpec {
+
+        public static final String BASE_PATH = ApiSpecMain.ROOT_PATH + "/friends";
+        public static final String TAG_NAME = "FRIEND";
+        public static final String TAG_DESCRIPTION = "Friend management";
+
+        private FriendSpec() {
+        }
+    }
+
+    public static final class AccountGroupSpec {
+
+        public static final String BASE_PATH = ApiSpecMain.ROOT_PATH + "/account-groups";
+        public static final String TAG_NAME = "ACCOUNT GROUP";
+        public static final String TAG_DESCRIPTION = "Account Group management";
+
+        private AccountGroupSpec() {
+        }
+    }
 }
