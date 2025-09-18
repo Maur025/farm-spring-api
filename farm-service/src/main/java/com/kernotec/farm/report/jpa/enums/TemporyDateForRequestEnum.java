0da292883e31c5e36c5a24a7924a80e0ae8267c2
Maur025 <mmoyapalli@gmail.com>
2025-09-18 18:01:43 -0400

    feat: change api summary

---
+++ farm-service/src/main/java/com/kernotec/farm/report/jpa/enums/TemporyDateForRequestEnum.java
@@ -0,0 +1,4 @@
+package com.kernotec.farm.report.jpa.enums;
+
+public enum TemporyDateForRequestEnum {
+}
---
+++ farm-service/src/main/java/com/kernotec/farm/report/rest/dto/request/ActivitySummaryByAccountRequest.java
@@ -0,0 +1,4 @@
+package com.kernotec.farm.report.rest.dto.request;
+
+public class ActivitySummaryByAccountRequest {
+}
