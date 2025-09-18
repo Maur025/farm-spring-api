b823c3cc136e743e71a06fa9eb650cf7999ebda7
Maur025 <mmoyapalli@gmail.com>
2025-09-18 18:01:43 -0400

    feat: change api summary

--- farm-service/src/main/java/com/kernotec/farm/report/rest/controller/ReportController.java
+++ farm-service/src/main/java/com/kernotec/farm/report/rest/controller/ReportController.java
@@ -3,16 +3,17 @@ package com.kernotec.farm.report.rest.controller;
 import com.kernotec.core.rest.dto.response.SingleResponse;
 import com.kernotec.farm.report.jpa.service.AccountReportService;
 import com.kernotec.farm.report.rest.ApiSpec.ReportSpec;
+import com.kernotec.farm.report.rest.dto.request.ActivitySummaryByAccountRequest;
 import com.kernotec.farm.report.rest.dto.response.account.ActivitySummaryResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,14 +26,18 @@ public class ReportController {
     private final AccountReportService accountReportService;
 
     @Operation(summary = "Get activity summary by account")
-    @GetMapping("account/{accountId}/activities/summary")
+    @PostMapping("account/{accountId}/activities/summary")
     @ResponseStatus(HttpStatus.OK)
     public SingleResponse<ActivitySummaryResponse> getActivitySummaryByAccount(
-        @PathVariable("accountId") UUID accountId, @RequestParam UUID socialNetworkId)
+        @PathVariable("accountId") UUID accountId,
+        @RequestBody ActivitySummaryByAccountRequest request)
     {
         return SingleResponse.<ActivitySummaryResponse>builder()
             .code(HttpStatus.OK.value())
-            .data(accountReportService.getActivitySummaryByAccountId(accountId, socialNetworkId))
+            .data(accountReportService.getActivitySummaryByAccountId(
+                accountId,
+                request.getSocialNetworkId()
+            ))
             .build();
     }
 }
