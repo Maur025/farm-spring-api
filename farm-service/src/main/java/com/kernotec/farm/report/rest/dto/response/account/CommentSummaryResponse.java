774275dd341df8a42dae22dac894e37bb4cd79b2
Maur025 <mmoyapalli@gmail.com>
2025-09-19 17:34:05 -0400

    feat: add publishing list and comment list in accouunt summary

--- farm-service/src/main/java/com/kernotec/farm/report/rest/dto/response/account/CommentSummaryResponse.java
+++ farm-service/src/main/java/com/kernotec/farm/report/rest/dto/response/account/CommentSummaryResponse.java
@@ -1,10 +1,24 @@
 package com.kernotec.farm.report.rest.dto.response.account;
 
+import java.util.List;
 import lombok.Builder;
 
 @Builder
 public record CommentSummaryResponse(Long totalComments, Long totalPositiveComments,
-                                     Long totalNegativeComments)
+                                     Long totalNegativeComments,
+                                     List<AccountSummaryTableResponse> comments)
 {
 
+    public CommentSummaryResponse(Long totalComments, Long totalPositiveComments,
+        Long totalNegativeComments)
+    {
+        this(totalComments, totalPositiveComments, totalNegativeComments, null);
+    }
+
+    public CommentSummaryResponse withComments(List<AccountSummaryTableResponse> comments) {
+        return new CommentSummaryResponse(
+            this.totalComments, this.totalPositiveComments,
+            this.totalNegativeComments, comments
+        );
+    }
 }
