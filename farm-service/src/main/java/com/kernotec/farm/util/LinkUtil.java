30bfcc66ef55429490791fd93f20b66952094ade
Maur025 <mmoyapalli@gmail.com>
2025-09-29 16:04:35 -0400

    feat: add extracting username from link

--- farm-service/src/main/java/com/kernotec/farm/util/LinkUtil.java
+++ farm-service/src/main/java/com/kernotec/farm/util/LinkUtil.java
@@ -1,8 +1,10 @@
 package com.kernotec.farm.util;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
 public class LinkUtil {
@@ -10,6 +12,10 @@ public class LinkUtil {
     public String getIdentityFacebookOfLink(String link) {
         if (link == null) {
             return null;
+        }
+
+        if (link.length() - 1 == link.lastIndexOf("/")) {
+            link = link.substring(0, link.lastIndexOf("/"));
         }
 
         String linkLastPart = link.substring(link.lastIndexOf('/') + 1);
